abc09bf0872feaaa383cf0e86498bbe8c4aa0f4e
Prakash Rao Vaddina <prakash.vaddina@gmail.com>
2020-12-25 16:08:47 +0100

    Extend the example with custom sinks

--- play/g3log/g3log-default-and-custom/main.cpp
+++ play/g3log/g3log-default-and-custom/main.cpp
@@ -26,6 +26,14 @@ struct CustomSink {
   }
 };
 
+struct StdOutSink {
+  void ReceiveLogMessage(g3::LogMessageMover logEntry) {
+     auto level = logEntry.get()._level;
+
+     std::cout << logEntry.get().toString() << std::endl;
+  }
+};
+
 int main()
 {
   auto worker { g3::LogWorker::createLogWorker() };
@@ -44,6 +52,14 @@ int main()
   LOG(INFO) << "CustomSink: This is an info log ..";
   LOG(DEBUG) << "CustomSink: This is a debug log ..";
   LOG(WARNING) << "CustomSink: This is a warning log ..";
+
+  //remove all the sinks and add only one default sink
+  worker->removeAllSinks();
+  auto stdOutHandle = worker->addSink(std::make_unique<StdOutSink>(),
+                                    &StdOutSink::ReceiveLogMessage);
+  LOG(INFO) << "StdoutSink: This is an info log ..";
+  LOG(DEBUG) << "StdoutSink: This is a debug log ..";
+  LOG(WARNING) << "StdoutSink: This is a warning log ..";
 
   // shutdown the log
   g3::internal::shutDownLogging();
